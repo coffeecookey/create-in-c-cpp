c664d439debdeb416a3ee6a0e6cef2a040f78843
coffeecookey <tanisha.ojha2004x2@gmail.com>
2026-04-28 22:58:30 +0530

    work in progress

---
+++ producer-consumer/pc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+// replace the word MAX with 100, wherever it appears in the code
+#define MAX 100
+
+struct
+
+int main(void){
+
+    return 0;
+}
